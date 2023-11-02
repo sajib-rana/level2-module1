4630a44b06b4a8a5e9d9c425f3c7a8d32af6d75a
sajib-rana <sajibrana444@gmail.com>
2023-11-02 22:13:32 +0600

    'nullish'

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -14,5 +14,26 @@
     const result1 = isAuthenticated ?? 'Guest';
     const result2 = isAuthenticated ? isAuthenticated : 'Guest';
     console.log({result1},{result2})
+
+    type User = {
+        name: string,
+        adrress:{
+            city:string,
+            road:string,
+            presentAdrress:string,
+            permanentAdrress?:string
+        }
+    }
+    const user: User = {
+        name:'sajib rana',
+        adrress:{
+            city:'dhaka',
+            road: 'three',
+            presentAdrress:'Mirpur-1'
+        }
+    }
+    const permanentAdrress = user?.adrress?.permanentAdrress ?? 'No permanent Adrress' 
+    // nullish operator null or undefine er opor depend kore dekhay tai ekhane undefine er poriborte No permanent Adrress
+    console.log({permanentAdrress})
     // 
 }
