ffe03ad2323334a070ae619294badd0e20d1b99c
sajib-rana <sajibrana444@gmail.com>
2023-11-05 14:45:13 +0600

    'conditional'

---
+++ module2/2.9.ts
@@ -0,0 +1,23 @@
+{
+    // conditional 
+
+    type a1 = number;
+    type b1 = undefined;
+    type x= a1 extends null ? true: false;
+    type y= a1 extends null ? true: b1 extends undefined? undefined: any;
+
+    type Sheikh = {
+        bike:string;
+        car:string;
+        ship:string;
+        plane:string;
+    }
+    
+    type CheckVehicle<T> = T extends keyof Sheikh ? true :false
+    
+    type Has = CheckVehicle<'ship'>
+
+
+    // 
+
+}
